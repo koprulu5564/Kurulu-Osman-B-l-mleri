484e5027c1d78c52d026f4285d17eef91047cb19
koprulu <159997439+koprulu5564@users.noreply.github.com>
2025-08-04 00:11:53 +0300

    Update generate_m3u.py

--- generate_m3u.py
+++ generate_m3u.py
@@ -7,6 +7,8 @@ PROXY_PREFIX = "https://stream-extractor.koprulu.workers.dev/?url="
 EXT = "&ext=mp4"
 COVER_ART = "https://iaatv.tmgrup.com.tr/71709e/500/268/0/0/500/268?u=https://iatv.tmgrup.com.tr/2024/09/24/500x268/1727165994263.jpg"
 CATEGORY = "Kuruluş Osman"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+EXTCL_OPT = "user-agent={}".format(USER_AGENT)
 
 def get_last_episode():
     try:
@@ -18,18 +20,21 @@ def get_last_episode():
 def check_episode(episode):
     try:
         url = f"{PROXY_PREFIX}{BASE_URL}{episode}-bolum/izle"
-        return requests.head(url, timeout=5).status_code == 200
+        headers = {"User-Agent": USER_AGENT}
+        return requests.head(url, headers=headers, timeout=5).status_code == 200
     except:
         return False
 
 def generate_m3u(end_episode):
-    m3u = f"""#EXTM3U
-#EXTINF:-1 group-title="{CATEGORY}",{CATEGORY}
+    m3u = f"""#EXTM3U x-tvg-url="http://botallen.com/epg.xml"
+#EXTM3U url-tvg="http://botallen.com/epg.xml"
+#EXTINF:-1 tvg-logo="{COVER_ART}" group-title="{CATEGORY}",{CATEGORY}
 {COVER_ART}\n"""
     
     for ep in range(1, end_episode + 1):
-        m3u += f"""#EXTINF:-1 tvg-id="KurulusOsman{ep}" tvg-name="Bölüm-{ep}" group-title="{CATEGORY}",Kuruluş Osman Bölüm-{ep}
-{PROXY_PREFIX}{BASE_URL}{ep}-bolum/izle{EXT}\n"""
+        stream_url = f"{PROXY_PREFIX}{BASE_URL}{ep}-bolum/izle{EXT}#{EXTCL_OPT}"
+        m3u += f"""#EXTINF:-1 tvg-id="KurulusOsman{ep}" tvg-name="Bölüm-{ep}" tvg-logo="{COVER_ART}" group-title="{CATEGORY}",Kuruluş Osman Bölüm-{ep}
+{stream_url}\n"""
     
     with open("kurulus-osman.m3u", "w", encoding="utf-8") as f:
         f.write(m3u)
